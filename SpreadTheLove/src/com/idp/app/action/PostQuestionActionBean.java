df5df25ef6d9602b290f809aa74f66ef0dc41506
siongmin <tansiongmin@gmail.com>
2014-03-13 16:38:38 +0800

    change import java for postquestionactionbean.java

--- SpreadTheLove/src/com/idp/app/action/PostQuestionActionBean.java
+++ SpreadTheLove/src/com/idp/app/action/PostQuestionActionBean.java
@@ -8,7 +8,7 @@ import net.sourceforge.stripes.action.DefaultHandler;
 import net.sourceforge.stripes.action.ForwardResolution;
 import net.sourceforge.stripes.action.Resolution;
 import net.sourceforge.stripes.action.UrlBinding;
-import java,
+
 
 @UrlBinding("/postquestion.action")
 public class PostQuestionActionBean extends BaseActionBean {
